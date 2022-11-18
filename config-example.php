57d62df2c9f8161b2b0053bf9fe371ad34406cf3
benjamin chen <benchen216@gmail.com>
2022-11-19 00:23:47 +0800

    Update config-example.php

--- config-example.php
+++ config-example.php
@@ -4,6 +4,6 @@ const DB_USER = "";
 const DB_PASS = "";
 const DB_NAME = "";
 const RESTRICT_URL = ""; # Restrict the plugin to only work on this domain
-const YOURLS_URL = ""; # YOURLS API URL
-const DIRECT_URL = ""; # Direct URL to the id page
+const YOURLS_URL = "https://example.com/yourls-api.php"; # YOURLS API URL
+const DIRECT_URL = "https://example.com/go.php?id="; # Direct URL to the id page
 const YOURLS_SIGNATURE = ""; # YOURLS Signature
